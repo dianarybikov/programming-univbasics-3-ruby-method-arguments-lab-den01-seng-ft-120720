b331e7482f01c46437907cbfad6523a4f347a147
Diana Rybikov <diana.ryb214@gmail.com>
2020-11-12 21:20:05 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,9 +8,9 @@ def introduction_with_language(name,language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction_with_language("Diana", "Ruby")
+introduction_with_language("Diana","Ruby")
 
-def introduction_with_language_optional(name,Ruby)
+def introduction_with_language_optional(name,"Ruby")
 puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
